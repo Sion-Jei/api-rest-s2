5c09c57904d6a0e876385ccdd7d2192deb7eb2d3
SionJei <sion-jei.mamanil@utem.cl>
2020-12-02 14:05:56 -0300

    agrego index js

--- src/index.js
+++ src/index.js
@@ -10,3 +10,5 @@ app.use(require('./routes/index'));//App requiere el contenido en index de route
 
 app.listen(3000);//Define el port en donde escucha app. El server arranca con "npm run dev"
 console.log('Server on port 3000');//Entrega un mensaje por consola sobre el estado del servidor
+
+//HOLAHOLA
